727ca063566ad54b9dbaedf3e20f514a6cfc9059
Olivier JM <manolivier93@gmail.com>
2019-01-20 19:39:59 +0200

    explained the unary function

--- unary.js
+++ unary.js
@@ -3,5 +3,10 @@ function unary(fn){
 	return function onlyOneArg(arg){
 		return fn(arg)
 }
+
 }
+// this one doesn't work because of the index that gets passed as the second args
+console.log(["1","2","3" ].map(parseInt))
+
+// the unary removes other arguments and maintains the first one only
 console.log(["1", "2", "3"].map(unary(parseInt)))
